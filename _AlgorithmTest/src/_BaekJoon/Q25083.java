e7ff9521540c82c35de83f5a20178dc5adb0e5e4
Innofactory <Innofactory@DESKTOP-KHJ0ML9>
2022-12-28 10:10:43 +0900

    백준 #25083 자바코드 제출

---
+++ _AlgorithmTest/src/_BaekJoon/Q25083.java
@@ -0,0 +1,22 @@
+package _BaekJoon;
+
+public class Q25083 {
+/*
+         ,r'"7 
+r`-_   ,'  ,/ 
+ \. ". L_r'
+   `~\/
+      |
+      |
+*/
+	public static void main(String args[]) {
+		
+		System.out.println("         ,r'\"7 ");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+		
+	}
+}
