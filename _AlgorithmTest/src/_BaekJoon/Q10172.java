aaa060090200d80e5f97364618acae8ec4e92890
Innofactory <Innofactory@DESKTOP-KHJ0ML9>
2022-12-28 10:36:31 +0900

    백준 #10172 자바코드 제출

---
+++ _AlgorithmTest/src/_BaekJoon/Q10172.java
@@ -0,0 +1,16 @@
+package _BaekJoon;
+
+public class Q10172 {
+/*
+ 개 출력
+ */
+	public static void main(String args[]) {
+		
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+		
+	}
+}
