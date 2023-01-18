a0cc529efc5e3b958a8fa9dca931dd3f39f933c1
Innofactory <Innofactory@DESKTOP-KHJ0ML9.mshome.net>
2023-01-18 11:22:41 +0900

    백준 #11653 주석 수정

--- _AlgorithmTest/src/_BaekJoon/Q11653.java
+++ _AlgorithmTest/src/_BaekJoon/Q11653.java
@@ -2,10 +2,9 @@ package _BaekJoon;
 
 import java.util.Scanner;
 
-public class Q11653 {
-	// 소인수분해
-	
+public class Q11653 {	
 	public static void main(String args[]) {
+		// 소인수분해
 		Scanner sc = new Scanner(System.in);
 		
 		int N = sc.nextInt();
