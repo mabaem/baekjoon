9a8be0a80e7389300dcfd34ffceb74882cf59ed1
Innofactory <Innofactory@DESKTOP-KHJ0ML9.mshome.net>
2023-01-20 10:40:11 +0900

    백준 #1427 자바코드 제출

---
+++ _AlgorithmTest/src/_BaekJoon/Q1427.java
@@ -0,0 +1,22 @@
+package _BaekJoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Q1427 {
+	public static void main(String args[]) {
+		// 소트인사이드
+		
+		Scanner sc = new Scanner(System.in);
+		
+		char[] charArray = sc.nextLine().toCharArray();
+		
+		Arrays.sort(charArray);
+		
+		for(int i = charArray.length - 1; i >= 0; i--) {
+			System.out.print(charArray[i]);
+		}
+		
+		sc.close();
+	}
+}
