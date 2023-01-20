af476d7b1fab1a568fcc349af2a17855769a3d13
Innofactory <Innofactory@DESKTOP-KHJ0ML9.mshome.net>
2023-01-20 12:05:31 +0900

    백준 #11651 자바코드 제출

--- _AlgorithmTest/src/_BaekJoon/Q11651.java
+++ _AlgorithmTest/src/_BaekJoon/Q11651.java
@@ -24,11 +24,11 @@ public class Q11651 {
 			@Override
 			public int compare(Integer[] o1, Integer[] o2) {
 				// TODO Auto-generated method stub
-				if (o1[0].equals(o2[0])) {
-					return o1[1] - o2[1];
+				if (o1[1].equals(o2[1])) {
+					return o1[0] - o2[0];
 				}
 				else {
-					return o1[0] - o2[0];
+					return o1[1] - o2[1];
 				}
 			}
 		
