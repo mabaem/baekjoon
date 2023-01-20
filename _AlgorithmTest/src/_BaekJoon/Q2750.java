e4475e2482bd756810b9f5c71729ba6ea0fe63ec
mabaem <jungyeji0110@naver.com>
2023-01-20 13:48:45 +0900

    Q2750 수정

--- _AlgorithmTest/src/_BaekJoon/Q2750.java
+++ _AlgorithmTest/src/_BaekJoon/Q2750.java
@@ -17,7 +17,7 @@ public class Q2750 {
 		}
 		
 		// 정렬부분
-		for(int i = N - 1; i > 0; i--) {
+		for(int i = N - 1; i > 0; i--) { 
 			for(int j = 0; j < i; j++) {
 				
 				if (array[j] > array[j+1]) {
