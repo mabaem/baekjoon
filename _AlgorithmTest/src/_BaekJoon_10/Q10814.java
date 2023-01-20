384a15d70c271f094354d8d66e776ae4b40c9a16
mabaem <jungyeji0110@naver.com>
2023-01-20 15:19:37 +0900

    백준 #10814 자바코드 제출

--- _AlgorithmTest/src/_BaekJoon_10/Q10814.java
+++ _AlgorithmTest/src/_BaekJoon_10/Q10814.java
@@ -1,7 +1,43 @@
 package _BaekJoon_10;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
 public class Q10814 {
 	public static void main(String args[]) {
 		// 나이순 정렬
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();	// 회원수
+
+		String[][] array = new String[n][2];
+		
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < 2; j++) {
+				array[i][j] = sc.next();
+			}
+		}
+		
+		// 나이순(오름차순), 이름(입력순)
+		Arrays.sort(array, new Comparator<String[]>() {
+
+			@Override
+			public int compare(String[] o1, String[] o2) {
+				// TODO Auto-generated method stub
+				
+				return Integer.compare(Integer.parseInt(o1[0]), Integer.parseInt(o2[0]));
+			}
+			
+		});
+		
+		for(int i=0;i<array.length;i++) {
+			
+			System.out.println(array[i][0]+" "+array[i][1]);
+	
+		}
+	
+		
+		sc.close();
 	}
 }
