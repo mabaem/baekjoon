d9c727d25deb2539c59f165f53b1e3f83bdb2749
mabaem <jungyeji0110@naver.com>
2023-01-20 13:47:29 +0900

    Q11650 수정

--- _AlgorithmTest/src/_BaekJoon/Q11650.java
+++ _AlgorithmTest/src/_BaekJoon/Q11650.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 import java.util.Scanner;
 
 public class Q11650 {
-	public static void main(String args[]) {
+	public static void main(String args[]) { 
 		// 좌표 정렬하기
 		Scanner sc = new Scanner(System.in);
 		
