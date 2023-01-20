852a36085dc35278c62793777d503e130325d7b0
mabaem <jungyeji0110@naver.com>
2023-01-20 13:48:09 +0900

    Q10989 수정

--- _AlgorithmTest/src/_BaekJoon/Q10989.java
+++ _AlgorithmTest/src/_BaekJoon/Q10989.java
@@ -12,7 +12,7 @@ public class Q10989 {
 		// 첫째 줄에 수의 개수 N(1 ≤ N ≤ 10,000,000)이 주어진다. 둘째 줄부터 N개의 줄에는 수가 주어진다. 이 수는 10,000보다 작거나 같은 자연수이다.
 		// 첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		StringBuilder sb = new StringBuilder();
 		
 		int n = Integer.parseInt(br.readLine());
