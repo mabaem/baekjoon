69519537d402949d21b99ec674aa692b7a2c2501
mabaem <jungyeji0110@naver.com>
2023-01-25 11:07:57 +0900

    백준 #2231 자바코드 제출

---
+++ _AlgorithmTest/src/_BaekJoon_10/Q2231.java
@@ -0,0 +1,40 @@
+package _BaekJoon_10;
+
+import java.util.Scanner;
+
+public class Q2231 {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();	// 분해합
+		
+		int min = 0;
+		
+		for(int i = 0; i < n; i++) {
+			
+			int sum = 0;
+			int x = i;
+			
+			while (x != 0) {
+				// 각 자리수 더하기
+				sum += (x % 10);
+				
+				// 1의 자리수가 될 때까지 10씩 나누기
+				x = x / 10;
+			}
+			
+			// 자기자신 더하기
+			sum += i;
+			
+			if (sum == n) {
+				min = i;
+				break;
+			}
+						
+		}
+		
+		System.out.println(min);
+				
+		sc.close();
+	}
+}
