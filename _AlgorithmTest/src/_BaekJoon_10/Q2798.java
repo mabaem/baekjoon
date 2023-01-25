afe42fad81992cf11f54cd38825e1ff184b02959
mabaem <jungyeji0110@naver.com>
2023-01-25 09:28:41 +0900

    백준 #2798 자바코드 제출

---
+++ _AlgorithmTest/src/_BaekJoon_10/Q2798.java
@@ -0,0 +1,58 @@
+package _BaekJoon_10;
+
+import java.util.Scanner;
+
+public class Q2798 {
+	public static void main(String agrs[]) {
+		// 블랙잭
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();	// 카드 개수
+		int m = sc.nextInt();	// 3장의 카드의 합의 최대값
+		
+		int[] cardArray = new int[n];	// 카드 배열
+		for(int i = 0; i < n; i++) {
+			cardArray[i] = sc.nextInt();
+		}
+		
+		int sum = 0, result = 0;	// sum:3장의 카드의 합, result:m과 가장 가까운 sum의 값
+		
+		for(int i = 0; i < n - 2; i++) {
+			
+			if (cardArray[i] > m) {
+				continue;
+			}
+			
+			for(int j = i + 1; j < n - 1; j++) {
+				
+				if (cardArray[i] + cardArray[j] > m) {
+					continue;
+				}
+								
+				for(int k = j + 1; k < n; k++) {
+					
+					sum = cardArray[i] + cardArray[j] + cardArray[k];
+					
+					if (sum > m) {
+						continue;
+					}
+					else {
+						
+						if (sum > result) {
+							result = sum;
+						}
+						
+					}
+					
+				}
+				
+			}
+			
+		}
+		
+		System.out.println(result);
+		
+		sc.close();
+	}
+}
