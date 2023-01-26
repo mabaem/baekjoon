8452e545b830da487f87380b9a22e4175664e5cf
mabaem <jungyeji0110@naver.com>
2023-01-26 10:13:48 +0900

    백준 #2447 자바코드 제출

---
+++ _AlgorithmTest/src/_BaekJoon_11/Q2447.java
@@ -0,0 +1,66 @@
+package _BaekJoon_11;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q2447 {
+	static char[][] array;
+	
+	public static void main(String[] args) throws IOException {
+		// 별 찍기
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		array = new char[N][N];
+		
+		drawStars(0, 0, N, false);
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				sb.append(array[i][j]);
+			}
+			sb.append('\n');
+		}
+		
+		System.out.print(sb);
+		
+		br.close();
+	}
+	
+	static void drawStars(int row, int column, int n, boolean blank) {
+		
+		if (blank) {
+			for(int i = row; i < row + n; i++) {
+				for(int j = column; j < column + n; j++) {
+					array[i][j] = ' ';				
+				}				
+			}
+			return;
+		}
+		
+		if (n == 1) {
+			array[row][column] = '*';
+			return;
+		}
+		
+		int size = n / 3;
+		int count = 0;
+		
+		for (int i = row; i < row + n; i += size) {
+			for (int j = column; j < column + n; j += size) {
+				count++;
+				
+				if (count == 5) { 
+					drawStars(i, j, size, true);
+				} else {
+					drawStars(i, j, size, false);
+				}
+			}
+		}
+		
+	}
+}
