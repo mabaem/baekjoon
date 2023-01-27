e21cf916b9d186f5560b1303564a2d76c6a33a3f
mabaem <93515120+mabaem@users.noreply.github.com>
2023-01-27 15:59:20 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1232 ms, Memory: 276868 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		Map<Integer, Integer> map = new HashMap<>();
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		for(int i = 0; i < N; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			
+			if (!map.containsKey(num)) {
+				map.put(num, 1);			
+			}
+			else {
+				map.put(num, map.get(num) + 1);
+			}
+		}
+		
+		int M = Integer.parseInt(br.readLine());
+		
+		st = new StringTokenizer(br.readLine());
+		
+		for(int i = 0; i < M; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			
+			if (map.containsKey(num)) {
+				bw.write(map.get(num)+" ");
+			}
+			else {
+				bw.write(0+" ");
+			}
+		}
+				
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
